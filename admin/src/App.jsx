88edaf125bf04e57a3cc8d599bb3f98c5705d6f9
sushil-thakur <122188969+sushil-thakur@users.noreply.github.com>
2025-07-27 13:23:51 +0545

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer} from 'react-toastify';
 
 const App = () => {
- const url = "http://localhost:4000";
+ const url = "https://foof-del-backend.onrender.com";
   return (
     <div>
       <ToastContainer/>
