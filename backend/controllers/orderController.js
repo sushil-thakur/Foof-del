94962f634f015ade5384392b9e4e68afe129fc1b
sushil-thakur <122188969+sushil-thakur@users.noreply.github.com>
2025-07-27 13:16:32 +0545

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order form frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://foof-del-frontend.onrender.com";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
